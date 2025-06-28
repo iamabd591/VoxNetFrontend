6daebc912e622e8ad6ba3c5cd15750518c184c21
iamabd591 <buttabdulhannan708@gmail.com>
2025-06-28 20:04:27 +0500

    cors intregration

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,23 @@ import chatRoutes from "./Src/Routes/chatRoutes.js";
 import cookieParser from "cookie-parser";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+const corsOption = {
+  methods: "GET, POST, PUT, DELETE",
+  optionsSuccessStatus: 204,
+  origin: process.env.CORS,
+  preflightContinue: false,
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOption));
 app.use("/auth/api", authRoutes);
 app.use("/user/api", userRoutes);
 app.use("/chat/api", chatRoutes);
